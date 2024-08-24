87bfb4b379c876b85cea9c1959690bfc9a09a90b
William Wollert <williamwollert@gmail.com>
2024-08-24 00:51:06 -0300

    v1.0

---
+++ Aula05/calculadora.js
@@ -0,0 +1,48 @@
+let opt = document.querySelector("#operacoes");
+let btn = document.getElementById("resultado");
+
+function getValue1() {
+  return parseFloat(document.getElementById("PriVal").value);
+}
+
+function getValue2() {
+  return parseFloat(document.getElementById("SegVal").value);
+}
+
+function getCalc() {
+  let result;
+  switch (opt.value) {
+    case "1":
+      result = som();
+    break;
+
+    case "2":
+      result = sub();
+    break;
+
+    case "3":
+      result = mlt();
+    break;
+
+    case "4":
+      result = div();
+    break;
+  }
+  document.getElementById("resultado").innerHTML = result;
+}
+
+function som() {
+  return getValue1() + getValue2();
+}
+
+function sub() {
+  return getValue1() - getValue2();
+}
+
+function mlt() {
+  return getValue1() * getValue2();
+}
+
+function div() {
+  return getValue1() / getValue2();
+}
